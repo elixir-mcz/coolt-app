096ec8c3c5bda61460ad813859ff5468a5ac8cdf
gildsonhugo <gildson.hugo@gmail.com>
2017-09-16 14:19:54 -0300

    mofifing component swipe

--- app/components/SwipeCards/SwipeCards.js
+++ app/components/SwipeCards/SwipeCards.js
@@ -1,134 +1,124 @@
-import React, { Component } from 'react'
-import Swiper from 'react-native-deck-swiper'
-import { AppRegistry, StyleSheet, View, Text, Button } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
-export default class SwipeCards extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      cards: ['1', '2', '3'],
-      swipedAllCards: false,
-      swipeDirection: '',
-      isSwipingBack: false,
-      cardIndex: 0
-    }
-  }
 
-  renderCard = card => {
+import SwipeCards from 'react-native-swipe-cards';
+
+let Card = React.createClass({
+  render() {
     return (
       <View style={styles.card}>
-        <Text style={styles.text}>{card}</Text>
+        <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+        <Text style={styles.text}>This is card {this.props.name}</Text>
       </View>
     )
-  };
-
-  onSwipedAllCards = () => {
-    this.setState({
-      swipedAllCards: true
-    })
-  };
-
-  swipeBack = () => {
-    if (!this.state.isSwipingBack) {
-      this.setIsSwipingBack(true, () => {
-        this.swiper.swipeBack(() => {
-          this.setIsSwipingBack(false)
-        })
-      })
-    }
-  };
-
-  setIsSwipingBack = (isSwipingBack, cb) => {
-    this.setState(
-      {
-        isSwipingBack: isSwipingBack
-      },
-      cb
-    )
-  };
-
-  jumpTo = () => {
-    this.swiper.swipeLeft()
-  };
-
-  render () {
-    return (
-      <View style={styles.container}>
-        <Swiper
-          ref={swiper => {
-            this.swiper = swiper
-          }}
-          onSwiped={this.onSwiped}
-          onTapCard={this.jumpTo}
-          cards={this.state.cards}
-          cardIndex={this.state.cardIndex}
-          cardVerticalMargin={80}
-          renderCard={this.renderCard}
-          onSwipedAll={this.onSwipedAllCards}
-          overlayLabels={{
-            bottom: {
-              title: 'BLEAH',
-              swipeColor: '#9262C2',
-              backgroundOpacity: '0.75',
-              fontColor: '#FFF'
-            },
-            left: {
-              title: 'NOPE',
-              swipeColor: '#FF6C6C',
-              backgroundOpacity: '0.75',
-              fontColor: '#FFF'
-            },
-            right: {
-              title: 'LIKE',
-              swipeColor: '#4CCC93',
-              backgroundOpacity: '0.75',
-              fontColor: '#FFF'
-            },
-            top: {
-              title: 'SUPER LIKE',
-              swipeColor: '#4EB8B7',
-              backgroundOpacity: '0.75',
-              fontColor: '#FFF'
-            }
-          }}
-          animateOverlayLabelsOpacity
-          animateCardOpacity
-        >
-          <Button onPress={this.swipeBack} title='Swipe Back' />
-          <Button onPress={this.jumpTo} title='Jump to last index' />
-        </Swiper>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  box1: {
-    flex: 1
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF'
-  },
-  card: {
-    flex: 1,
-    borderRadius: 4,
-    borderWidth: 2,
-    borderColor: '#E8E8E8',
-    justifyContent: 'center',
-    backgroundColor: 'white'
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 50,
-    backgroundColor: 'transparent'
-  },
-  done: {
-    textAlign: 'center',
-    fontSize: 30,
-    color: 'white',
-    backgroundColor: 'transparent'
   }
 })
 
-AppRegistry.registerComponent('SwipeCards', () => SwipeCards);
+let NoMoreCards = React.createClass({
+  render() {
+    return (
+      <View style={styles.noMoreCards}>
+        <Text>No more cards</Text>
+      </View>
+    )
+  }
+})
+
+const Cards = [
+  {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+  {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+  {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+  {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+  {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+  {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+  {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+  {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+  {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+]
+
+const Cards2 = [
+  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+]
+
+export default React.createClass({
+  getInitialState() {
+    return {
+      cards: Cards,
+      outOfCards: false
+    }
+  },
+  handleYup (card) {
+    console.log("yup")
+  },
+  handleNope (card) {
+    console.log("nope")
+  },
+  cardRemoved (index) {
+    console.log(`The index is ${index}`);
+
+    let CARD_REFRESH_LIMIT = 3
+
+    if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
+      console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
+
+      if (!this.state.outOfCards) {
+        console.log(`Adding ${Cards2.length} more cards`)
+
+        this.setState({
+          cards: this.state.cards.concat(Cards2),
+          outOfCards: true
+        })
+      }
+
+    }
+
+  },
+  render() {
+    return (
+      <SwipeCards
+        cards={this.state.cards}
+        loop={false}
+
+        renderCard={(cardData) => <Card {...cardData} />}
+        renderNoMoreCards={() => <NoMoreCards />}
+        showYup={true}
+        showNope={true}
+
+        handleYup={this.handleYup}
+        handleNope={this.handleNope}
+        cardRemoved={this.cardRemoved}
+      />
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  card: {
+    alignItems: 'center',
+    borderRadius: 5,
+    overflow: 'hidden',
+    borderColor: 'grey',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    elevation: 1,
+  },
+  thumbnail: {
+    flex: 1,
+    width: 300,
+    height: 300,
+  },
+  text: {
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  noMoreCards: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
--- index.android.js
+++ index.android.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 
-import SwipeCards from './app/components/SwipeCards/SwipeCards';
+import Cards from './app/components/SwipeCards/SwipeCards';
 
 export default class cooltApp extends Component{
   render(){
     return(
       <View style={{flex: 1}}>
-        <SwipeCards />
+        <Cards />
       </View>
     );
   }
